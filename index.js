f5faea3a167635413fc4d7bc4e76c526896374cd
Kykeon <paulinoandresmiguel@gmail.com>
2022-02-15 22:23:47 -0500

    Attempted and added get request using Axios
    Used async for first time

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
+// const express = require('express')
+// const app = express()
+// const port = 8000
+// const path = require('path')
 
-const express = require('express')
-const app = express()
-const port = 8000
-const path = require('path')
+// let counter = 1;
 
-let counter = 1;
+// app.get('/home', (req, res) => {
+//     res.sendFile(path.join(__dirname, './index.html'))
+// })
 
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'))
-})
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname, './about.html'))
+// })
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, './about.html'))
-})
+// app.get('/contact', (req, res) => {
+//     res.sendFile(path.join(__dirname, './contact.html'))
+// })
 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, './contact.html'))
-})
+// app.get('/product', (req, res) => {
+//     res.sendFile(path.join(__dirname, './products.html'))
+// })
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
-app.get('/product', (req, res) => {
-    res.sendFile(path.join(__dirname, './products.html'))
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const axios = require("axios");
+
+// Make a request for a user with a given ID
+getUser();
+
+async function getUser() {
+  try {
+    const response = await axios.get("https://api.punkapi.com/v2/beers");
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+    }
+    
+    console.log("Testing");
+}
+
+
